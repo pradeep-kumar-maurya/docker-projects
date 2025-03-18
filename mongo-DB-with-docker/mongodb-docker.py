ea47e1c5803a9dfeec94c0e2a7681f67e4b54ff4
Pradeep Kumar Maurya <pk454649@gmail.com>
2025-03-19 02:23:03 +0530

    docker volumes

--- mongo-DB-with-docker/mongodb-docker.py
+++ mongo-DB-with-docker/mongodb-docker.py
@@ -9,37 +9,38 @@ mongo_uri = f'mongodb://{mongo_username}:{mongo_password}@{mongo_db_host}:{mongo
 
 # connect to MongoDB
 client = MongoClient(mongo_uri)
+print(client)
 
 # create (or access) a database
-db = client['my_database']
-
-# create (or access) a collection
-collection = db['my_collection']
-
-# insert one document in collection
-collection.insert_one(
-    {"name": "Pradeep", "age": 26, "city": "Hyderabad"}
-)
-
-# insert multiple documents in collection
-collection.insert_many([
-    {"name": "Alice", "age": 30, "city": "NYC"},
-    {"name": "Bob", "age": 35, "city": "LA"},
-])
+# db = client['my_database']
+#
+# # create (or access) a collection
+# collection = db['my_collection']
+#
+# # insert one document in collection
+# collection.insert_one(
+#     {"name": "Pradeep", "age": 26, "city": "Hyderabad"}
+# )
+#
+# # insert multiple documents in collection
+# collection.insert_many([
+#     {"name": "Alice", "age": 30, "city": "NYC"},
+#     {"name": "Bob", "age": 35, "city": "LA"},
+# ])
 
 # get databases
 databases = client.list_database_names()
 print("DBs in MongoDB ->", databases)
 
 # find one document
-one_doc = collection.find_one({'name': 'Pradeep'})
-print("individual document ->", one_doc)
-
-# find all documents
-all_docs = collection.find({})
-print("All documents")
-for doc in all_docs:
-    print(doc)
+# one_doc = collection.find_one({'name': 'Pradeep'})
+# print("individual document ->", one_doc)
+#
+# # find all documents
+# all_docs = collection.find({})
+# print("All documents")
+# for doc in all_docs:
+#     print(doc)
 
 
 client.close()
